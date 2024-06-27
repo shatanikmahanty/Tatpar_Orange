28483a92c24117e33e29eedf307365c3d8053e42
KALYANIKILAPARTHI <kilaparthik@gmail.com>
2024-06-27 10:26:12 +0530

    Added code to rectify the navigation error (back button)

--- lib/features/case/data/repos/case_repo.dart
+++ lib/features/case/data/repos/case_repo.dart
@@ -22,8 +22,8 @@ class CaseRepo {
   Future<ReferralDetailsModel> saveReferralDetails(
       {required ReferralDetailsModel referralDetailsModel,
       required int? id}) async {
-    // Box<ReferralDetailsModel> dataBox =
-    //     Hive.box<ReferralDetailsModel>('referralDetailsModel');
+    Box<ReferralDetailsModel> dataBox =
+        Hive.box<ReferralDetailsModel>('referralDetailsPostModel');
     final request = NetworkRequest(
       '$referralDetailsUrl${id == null ? '' : '/$id'}',
       id == null ? RequestMethod.post : RequestMethod.patch,
@@ -35,16 +35,25 @@ class CaseRepo {
     );
     final result = await NetworkManager.instance.perform(request);
     if (result.status == Status.ok) {
+      Box<ReferralDetailsModel> dataBox =
+          Hive.box<ReferralDetailsModel>('referralDetailsGetModel');
+          await dataBox.put('referralModels', ReferralDetailsModel.fromJson(result.data['data']))
+
       AuthCubit.instance.caseId = result.data['data']['case_id'];
       // updateCase(AuthCubit.instance.workingCaseId ?? 0, {
       //   'referral': result.data['case_id'],
       // });
       return ReferralDetailsModel.fromJson(result.data['data']);
     } else {
-      throw ApplicationError(
-        errorMsg: 'Error submitting data',
-        type: Unauthorized(),
-      );
+      if (result.error != null && result.error?.type is NetworkError) {
+        // log('Using stored data from Hive: $storedData');
+        // return storedData;
+      } else {
+        throw ApplicationError(
+          errorMsg: 'Error fetching data',
+          type: UnExpected(),
+        );
+      }
     }
   }
 
