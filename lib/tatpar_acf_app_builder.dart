c870780baf36857eae74db8a873e3893b3b8c475
KALYANIKILAPARTHI <kilaparthik@gmail.com>
2024-08-02 15:36:10 +0530

    Commented out code for force Update

--- lib/tatpar_acf_app_builder.dart
+++ lib/tatpar_acf_app_builder.dart
@@ -8,7 +8,6 @@ import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
 
 import 'package:tatpar_acf/features/app/presentation/builders/app_responsive_layout_builder.dart';
-import 'package:tatpar_acf/features/appProvider/app_provider.dart';
 import 'package:tatpar_acf/features/authentication/blocs/auth_cubit.dart';
 import 'package:tatpar_acf/features/authentication/data/repo/auth_repo.dart';
 import 'package:tatpar_acf/features/authentication/presentation/listeners/login_listener_wrapper.dart';
@@ -34,155 +33,153 @@ class TatparAcfAppBuilder extends AppBuilder {
     required LanguageProvider provider,
     final String? initialDeepLink,
   }) : super(
-            onInitState: (context) {},
-            repositoryProviders: [
-              RepositoryProvider<AppLinksRepository>.value(
-                value: appLinksRepository,
-              ),
-              RepositoryProvider<CaseRepo>(
-                create: (context) => CaseRepo(),
-              ),
-              RepositoryProvider<SourceRepo>(
-                create: (context) => SourceRepo(),
-              ),
-              RepositoryProvider<AuthRepo>(
-                create: (context) => AuthRepo(FirebaseAuth.instance),
-              ),
-              RepositoryProvider<ReferralDetailsRepository>(
-                create: (context) => ReferralDetailsRepository(),
-              )
-            ],
-            providers: [
-              BlocProvider<AppCubit>(
-                create: (context) => AppCubit.instance,
-              ),
-              BlocProvider<AuthCubit>(
-                create: (context) => AuthCubit.instance
-                  ..init(
-                    context.read<AuthRepo>(),
-                  ),
-                lazy: false,
-              ),
-
-              // BlocProvider<TBScreeningStateCubit>(
-              //   create: (context) => TBScreeningStateCubit(),
-              // ),
-              BlocProvider<AppLinksCubit>(
-                create: (context) => AppLinksCubit(
-                  null,
-                  context.read<AppLinksRepository>(),
+          onInitState: (context) {},
+          repositoryProviders: [
+            RepositoryProvider<AppLinksRepository>.value(
+              value: appLinksRepository,
+            ),
+            RepositoryProvider<CaseRepo>(
+              create: (context) => CaseRepo(),
+            ),
+            RepositoryProvider<SourceRepo>(
+              create: (context) => SourceRepo(),
+            ),
+            RepositoryProvider<AuthRepo>(
+              create: (context) => AuthRepo(FirebaseAuth.instance),
+            ),
+            RepositoryProvider<ReferralDetailsRepository>(
+              create: (context) => ReferralDetailsRepository(),
+            )
+          ],
+          providers: [
+            BlocProvider<AppCubit>(
+              create: (context) => AppCubit.instance,
+            ),
+            BlocProvider<AuthCubit>(
+              create: (context) => AuthCubit.instance
+                ..init(
+                  context.read<AuthRepo>(),
                 ),
-                lazy: false,
-              ),
-              BlocProvider<SourceCubit>(
-                create: (context) => SourceCubit(
-                  context.read<SourceRepo>(),
-                ),
-              ),
-              // BlocProvider<SubordinatesCaseCubit>(
-              //     create: (context) => SubordinatesCaseCubit()),
-              // BlocProvider(
-              //   create: (_) => SplashBloc(),
-              // ),
-              // BlocProvider<ContactTracingCubit>(
-              //   create: (context) => ContactTracingCubit(
-              //     repo: context.read<CaseRepo>(),
-              //     contactTracingModel: const ContactTracingModel(),
-              //   ),
-              // ),
+              lazy: false,
+            ),
 
-              BlocProvider<CaseListCubit>(
-                create: (context) => CaseListCubit(
-                  caseRepo: context.read<CaseRepo>(),
-                ),
+            // BlocProvider<TBScreeningStateCubit>(
+            //   create: (context) => TBScreeningStateCubit(),
+            // ),
+            BlocProvider<AppLinksCubit>(
+              create: (context) => AppLinksCubit(
+                null,
+                context.read<AppLinksRepository>(),
               ),
-              BlocProvider<CaseCubit>(
-                create: (context) => CaseCubit(
-                    caseRepo: context.read<CaseRepo>(),
-                    caseModel: const Case()),
-              )
-            ],
-            builder: (context) => LoginListenerWrapper(
-                  initialUser: context.read<AuthCubit>().state.user,
-                  onLogin: (context, user) {
-                    ///For performing tasks after login
-                    context.read<AuthCubit>().loadUserDetails();
+              lazy: false,
+            ),
+            BlocProvider<SourceCubit>(
+              create: (context) => SourceCubit(
+                context.read<SourceRepo>(),
+              ),
+            ),
+            // BlocProvider<SubordinatesCaseCubit>(
+            //     create: (context) => SubordinatesCaseCubit()),
+            // BlocProvider(
+            //   create: (_) => SplashBloc(),
+            // ),
+            // BlocProvider<ContactTracingCubit>(
+            //   create: (context) => ContactTracingCubit(
+            //     repo: context.read<CaseRepo>(),
+            //     contactTracingModel: const ContactTracingModel(),
+            //   ),
+            // ),
 
-                    context.read<SourceCubit>().loadDistricts();
-                    context.read<SourceCubit>().loadDiagnosisData();
+            BlocProvider<CaseListCubit>(
+              create: (context) => CaseListCubit(
+                caseRepo: context.read<CaseRepo>(),
+              ),
+            ),
+            BlocProvider<CaseCubit>(
+              create: (context) => CaseCubit(
+                  caseRepo: context.read<CaseRepo>(), caseModel: const Case()),
+            )
+          ],
+          builder: (context) => LoginListenerWrapper(
+            initialUser: context.read<AuthCubit>().state.user,
+            onLogin: (context, user) {
+              ///For performing tasks after login
+              context.read<AuthCubit>().loadUserDetails();
 
-                    context.read<CaseListCubit>().getCasesForHealthWorker();
-                  },
-                  onLogout: (context) {},
-                  child: ChangeNotifierProvider<AppProvider>(
-                    create: (_) => AppProvider(),
-                    child: AppCubitConsumer(
-                        listenWhen: (previous, current) =>
-                            previous.environment != current.environment,
-                        listener: (context, state) async {},
-                        builder: (context, appState) {
-                          return MaterialApp.router(
-                            debugShowCheckedModeBanner: false,
-                            scaffoldMessengerKey:
-                                DjangoflowAppSnackbar.scaffoldMessengerKey,
-                            title: appName,
-                            routeInformationParser:
-                                appRouter.defaultRouteParser(),
-                            theme: AppTheme.light,
-                            darkTheme: AppTheme.dark,
-                            themeMode: appState.themeMode,
-                            // locale: Locale(appState.locale, ''),
-                            locale: provider.locale,
-                            supportedLocales: L10n.all,
-                            localizationsDelegates: const [
-                              AppLocalizations.delegate,
-                              GlobalMaterialLocalizations.delegate,
-                              GlobalWidgetsLocalizations.delegate,
-                              GlobalCupertinoLocalizations.delegate,
-                            ],
-                            routerDelegate: appRouter.delegate(
-                              deepLinkBuilder: (_) => initialDeepLink != null
-                                  ? DeepLink.path(initialDeepLink)
-                                  : const DeepLink(
-                                      [
-                                        AppHomeRoute(),
-                                      ],
-                                    ),
-                              // List of global navigation obsersers here
-                              // SentryNavigationObserver
-                              // navigatorObservers: () => {RouteObserver()},
+              context.read<SourceCubit>().loadDistricts();
+              context.read<SourceCubit>().loadDiagnosisData();
+
+              context.read<CaseListCubit>().getCasesForHealthWorker();
+            },
+            onLogout: (context) {},
+            // child: ChangeNotifierProvider<AppProvider>(
+            //   create: (_) => AppProvider(),
+            child: AppCubitConsumer(
+                listenWhen: (previous, current) =>
+                    previous.environment != current.environment,
+                listener: (context, state) async {},
+                builder: (context, appState) {
+                  return MaterialApp.router(
+                    debugShowCheckedModeBanner: false,
+                    scaffoldMessengerKey:
+                        DjangoflowAppSnackbar.scaffoldMessengerKey,
+                    title: appName,
+                    routeInformationParser: appRouter.defaultRouteParser(),
+                    theme: AppTheme.light,
+                    darkTheme: AppTheme.dark,
+                    themeMode: appState.themeMode,
+                    // locale: Locale(appState.locale, ''),
+                    locale: provider.locale,
+                    supportedLocales: L10n.all,
+                    localizationsDelegates: const [
+                      AppLocalizations.delegate,
+                      GlobalMaterialLocalizations.delegate,
+                      GlobalWidgetsLocalizations.delegate,
+                      GlobalCupertinoLocalizations.delegate,
+                    ],
+                    routerDelegate: appRouter.delegate(
+                      deepLinkBuilder: (_) => initialDeepLink != null
+                          ? DeepLink.path(initialDeepLink)
+                          : const DeepLink(
+                              [
+                                AppHomeRoute(),
+                              ],
                             ),
-                            builder: (context, child) =>
-                                AppResponsiveLayoutBuilder(
-                              child: SandboxBanner(
-                                isSandbox: appState.environment ==
-                                    AppEnvironment.sandbox,
-                                child: child != null
-                                    ? kIsWeb
-                                        ? child
-                                        : AppLinksCubitListener(
-                                            listenWhen: (previous, current) =>
-                                                current != null,
-                                            listener: (context, appLink) {
-                                              final path = appLink?.path;
-                                              if (path != null) {
-                                                appRouter.navigateNamed(
-                                                  path,
-                                                  onFailure: (failure) {
-                                                    appRouter.navigate(
-                                                      const AppHomeRoute(),
-                                                    );
-                                                  },
-                                                );
-                                              }
-                                            },
-                                            child: child,
-                                          )
-                                    : const Offstage(),
-                              ),
-                            ),
-                          );
-                        }),
-                  ),
-                ));
+                      // List of global navigation obsersers here
+                      // SentryNavigationObserver
+                      // navigatorObservers: () => {RouteObserver()},
+                    ),
+                    builder: (context, child) => AppResponsiveLayoutBuilder(
+                      child: SandboxBanner(
+                        isSandbox:
+                            appState.environment == AppEnvironment.sandbox,
+                        child: child != null
+                            ? kIsWeb
+                                ? child
+                                : AppLinksCubitListener(
+                                    listenWhen: (previous, current) =>
+                                        current != null,
+                                    listener: (context, appLink) {
+                                      final path = appLink?.path;
+                                      if (path != null) {
+                                        appRouter.navigateNamed(
+                                          path,
+                                          onFailure: (failure) {
+                                            appRouter.navigate(
+                                              const AppHomeRoute(),
+                                            );
+                                          },
+                                        );
+                                      }
+                                    },
+                                    child: child,
+                                  )
+                            : const Offstage(),
+                      ),
+                    ),
+                  );
+                }),
+          ),
+          //)
+        );
 }
